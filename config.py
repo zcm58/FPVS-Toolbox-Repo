f340a4c747dcbf5b7a93c6b40316a7646283c4ae
Zack <zcm58@msstate.edu>
2025-04-30 13:02:10 -0500

    Updated config file to new repo directory.

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import numpy as np
 import customtkinter as ctk
 
 # — FPVS Toolbox version & update API —
-FPVS_TOOLBOX_VERSION       = "0.7.1"
-FPVS_TOOLBOX_UPDATE_API    = "https://api.github.com/repos/zcm58/FPVSRepository/releases/latest"
-FPVS_TOOLBOX_REPO_PAGE     = "https://github.com/zcm58/FPVSRepository/releases"
+FPVS_TOOLBOX_VERSION       = "0.7.0"
+FPVS_TOOLBOX_UPDATE_API    = "https://api.github.com/repos/zcm58/FPVS-Toolbox-Repo/releases/latest"
+FPVS_TOOLBOX_REPO_PAGE     = "https://github.com/zcm58/FPVS-Toolbox-Repo/releases"
 
 # — metrics —
 TARGET_FREQUENCIES = np.arange(1.2, 16.8 + 1.2, 1.2)
