972852f57827c071bab41f3b38a64c9f5b1f2dab
Zack <zcm58@msstate.edu>
2025-05-12 11:14:06 -0500

    Directed Compiler Script.py to src directory.

--- src/Misc/Compiler Script.py
+++ src/Misc/Compiler Script.py
@@ -1,13 +1,17 @@
+cd "C:\Users\zcm58\PycharmProjects\FPVS Toolbox Repo"
+
 pyinstaller `
   --clean `
   --noconfirm `
   --windowed `
-  -n FPVS_Toolbox_0.8.2 `
-  -i "C:\Users\zcm58\OneDrive - Mississippi State University\Office Desktop\App Icon.png" `
+  --name FPVS_Toolbox_0.8.2 `
+  --icon "C:\Users\zcm58\OneDrive - Mississippi State University\Office Desktop\AppIcon.ico" `
   --collect-all mne `
-  --hidden-import=mne.io.bdf `
-  --hidden-import=mne.io.eeglab `
-  --hidden-import=scipy `
-  --hidden-import=pandas `
-  --hidden-import=numpy `
-  -F main.py
+  --hidden-import mne.io.bdf `
+  --hidden-import mne.io.eeglab `
+  --hidden-import scipy `
+  --hidden-import pandas `
+  --hidden-import numpy `
+  --paths .\src `
+  -F `
+  .\src\main.py
