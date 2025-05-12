ac93feb722cc72c86f10516a5dd4c2b7a2becf0f
Zack <zcm58@msstate.edu>
2025-05-12 10:49:10 -0500

    Updated to v0.8.2.

--- src/Misc/Compiler Script.py
+++ src/Misc/Compiler Script.py
@@ -2,7 +2,7 @@ pyinstaller `
   --clean `
   --noconfirm `
   --windowed `
-  -n FPVS_Toolbox_0.8.0 `
+  -n FPVS_Toolbox_0.8.2 `
   -i "C:\Users\zcm58\OneDrive - Mississippi State University\Office Desktop\App Icon.png" `
   --collect-all mne `
   --hidden-import=mne.io.bdf `
