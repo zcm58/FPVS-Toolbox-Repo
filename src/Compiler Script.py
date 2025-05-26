78d3f1f1f3cd6785741cb8086eeef1c7fcb284ec
zackm <zcm58@msstate.edu>
2025-05-26 15:02:31 -0500

    Updated Compiler Script.py to work with restructure

--- src/Compiler Script.py
+++ src/Compiler Script.py
@@ -1,4 +1,14 @@
-pyinstaller --clean --noconfirm --windowed -n FPVS_Toolbox_0.8.2 `
+pyinstaller `
+  --clean `
+  --noconfirm `
+  --windowed `
+  -n FPVS_Toolbox_0.8.2 `
+  --paths=src `
   -i "C:\Users\zackm\OneDrive - Mississippi State University\Office Desktop\ToolBox Icon.ico" `
-  --collect-all mne --hidden-import=mne.io.bdf --hidden-import=mne.io.eeglab `
-  --hidden-import=scipy --hidden-import=pandas --hidden-import=numpy -F src/main.py
+  --collect-all mne `
+  --hidden-import=mne.io.bdf `
+  --hidden-import=mne.io.eeglab `
+  --hidden-import=scipy `
+  --hidden-import=pandas `
+  --hidden-import=numpy `
+  -F src/main.py
