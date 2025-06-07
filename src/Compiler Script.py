0ef5b63b35ed7086abafce54703ba1c89989ed77
zackm <zcm58@msstate.edu>
2025-06-07 15:08:02 -0500

    Updated to v0.9.8

--- src/Compiler Script.py
+++ src/Compiler Script.py
@@ -2,7 +2,7 @@ pyinstaller `
   --clean `
   --noconfirm `
   --windowed `
-  -n FPVS_Toolbox_0.9.7  `
+  -n FPVS_Toolbox_0.9.8 `
   --paths=src `
   -i "C:\Users\zackm\OneDrive - Mississippi State University\Office Desktop\ToolBox Icon.ico" `
   --collect-all mne `
