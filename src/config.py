f977dfcf5a85afadfef427da971b2c45807de239
zackm <zcm58@msstate.edu>
2025-06-08 18:52:35 -0500

    Updated to v.0.9.9

--- src/config.py
+++ src/config.py
@@ -4,7 +4,7 @@ from Main_App.settings_manager import SettingsManager
 
 # — FPVS Toolbox version & update API —
 
-FPVS_TOOLBOX_VERSION       = "0.9.8"
+FPVS_TOOLBOX_VERSION       = "0.9.9"
 
 FPVS_TOOLBOX_UPDATE_API    = "https://api.github.com/repos/zcm58/FPVS-Toolbox-Repo/releases/latest"
 FPVS_TOOLBOX_REPO_PAGE     = "https://github.com/zcm58/FPVS-Toolbox-Repo/releases"
