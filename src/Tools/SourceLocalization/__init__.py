81f2a93f3f552e70ea63fe26188415b1937641da
Zack Murphy <zcm58@msstate.edu>
2025-06-25 17:00:22 -0500

    Add GUI option for averaging LORETA results

--- src/Tools/SourceLocalization/__init__.py
+++ src/Tools/SourceLocalization/__init__.py
@@ -1,7 +1,19 @@
 """Source localization tools using (s/e)LORETA."""
 
 from .eloreta_gui import SourceLocalizationWindow
-from .runner import run_source_localization
+from .runner import (
+    run_source_localization,
+    average_stc_files,
+    average_stc_directory,
+    average_conditions_dir,
+)
 from .visualization import view_source_estimate
 
-__all__ = ["SourceLocalizationWindow", "run_source_localization", "view_source_estimate"]
+__all__ = [
+    "SourceLocalizationWindow",
+    "run_source_localization",
+    "view_source_estimate",
+    "average_stc_files",
+    "average_stc_directory",
+    "average_conditions_dir",
+]
