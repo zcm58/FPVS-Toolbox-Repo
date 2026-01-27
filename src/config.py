68eb5edacae8c8153e7c9af400bc59a5726ba7ff
Zack <zcm58@msstate.edu>
2026-01-27 08:30:21 -0600

    Updated to v1.6.0

--- src/config.py
+++ src/config.py
@@ -15,7 +15,7 @@ from __future__ import annotations
 import numpy as np
 
 # — Version & repo —
-FPVS_TOOLBOX_VERSION: str = "1.5.3"
+FPVS_TOOLBOX_VERSION: str = "1.6.0"
 FPVS_TOOLBOX_UPDATE_API: str = (
     "https://api.github.com/repos/zcm58/FPVS-Toolbox-Repo/releases/latest"
 )
